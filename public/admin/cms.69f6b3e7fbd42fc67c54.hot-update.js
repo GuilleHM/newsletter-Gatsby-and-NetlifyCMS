5ad23d552efb3982ad81c92c4971d1754d60adcc
GuilleHM <guillehm1@outlook.com>
2020-05-30 18:24:31 +0200

    Carpeta de administración del CMS fuera de .gitignore

---
+++ public/admin/cms.69f6b3e7fbd42fc67c54.hot-update.js
@@ -0,0 +1,348 @@
+webpackHotUpdate("cms",{
+
+/***/ "./node_modules/typeface-amiko/index.css":
+/*!***********************************************!*\
+  !*** ./node_modules/typeface-amiko/index.css ***!
+  \***********************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+    if(true) {
+      // 1590850580484
+      var cssReload = __webpack_require__(/*! ./node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js */ "./node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js")(module.i, {"hmr":true,"locals":false});
+      module.hot.dispose(cssReload);
+      module.hot.accept(undefined, cssReload);
+    }
+  
+
+/***/ }),
+
+/***/ "./src/components/all.sass":
+/*!*********************************!*\
+  !*** ./src/components/all.sass ***!
+  \*********************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+    if(true) {
+      // 1590850587254
+      var cssReload = __webpack_require__(/*! ./node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js */ "./node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js")(module.i, {"hmr":true,"locals":false});
+      module.hot.dispose(cssReload);
+      module.hot.accept(undefined, cssReload);
+    }
+  
+
+/***/ }),
+
+/***/ "./src/templates/index-page.js":
+/*!*************************************!*\
+  !*** ./src/templates/index-page.js ***!
+  \*************************************/
+/*! exports provided: IndexPageTemplate, default, pageQuery */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* WEBPACK VAR INJECTION */(function(module) {/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "IndexPageTemplate", function() { return IndexPageTemplate; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "pageQuery", function() { return pageQuery; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "react");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var prop_types__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! prop-types */ "./node_modules/prop-types/index.js");
+/* harmony import */ var prop_types__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(prop_types__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var gatsby__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! gatsby */ "./.cache/gatsby-browser-entry.js");
+/* harmony import */ var _components_Layout__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../components/Layout */ "./src/components/Layout.js");
+/* harmony import */ var _components_NuevoComponente__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../components/NuevoComponente */ "./src/components/NuevoComponente.js");
+/* harmony import */ var _components_MyNewIndexPageComponent__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../components/MyNewIndexPageComponent */ "./src/components/MyNewIndexPageComponent.js");
+var _this = undefined,
+    _jsxFileName = "C:\\Users\\GuilleHM\\Desktop\\newsletter-Gatsby-and-NetlifyCMS\\src\\templates\\index-page.js";
+
+(function () {
+  var enterModule = typeof reactHotLoaderGlobal !== 'undefined' ? reactHotLoaderGlobal.enterModule : undefined;
+  enterModule && enterModule(module);
+})();
+
+var __signature__ = typeof reactHotLoaderGlobal !== 'undefined' ? reactHotLoaderGlobal.default.signature : function (a) {
+  return a;
+};
+
+/* eslint-disable */
+
+
+
+
+
+ //import Features from '../components/Features'
+//import BlogRoll from '../components/BlogRoll'
+
+var IndexPageTemplate = function IndexPageTemplate(_ref) {
+  var title = _ref.title,
+      subheading = _ref.subheading,
+      paragraph = _ref.paragraph,
+      multipleItems = _ref.multipleItems;
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 18,
+      columnNumber: 5
+    }
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "margin-top-0",
+    style: {
+      textAlign: "center",
+      padding: "72px"
+    },
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 19,
+      columnNumber: 7
+    }
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    style: {
+      display: "flex",
+      height: "150px",
+      lineHeight: "1",
+      justifyContent: "space-around",
+      alignItems: "left",
+      flexDirection: "column"
+    },
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 26,
+      columnNumber: 9
+    }
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
+    className: "has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen",
+    style: {
+      color: "black",
+      lineHeight: "1",
+      padding: "0.25em"
+    },
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 36,
+      columnNumber: 11
+    }
+  }, title), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", {
+    className: "has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen",
+    style: {
+      color: "#717171",
+      lineHeight: "1",
+      padding: "0.25em"
+    },
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 46,
+      columnNumber: 11
+    }
+  }, subheading), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    className: "parrafo-guille",
+    style: {
+      color: "#0000ff",
+      lineHeight: "1",
+      margin: "1rem auto",
+      padding: "0.25em"
+    },
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 56,
+      columnNumber: 11
+    }
+  }, paragraph), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_NuevoComponente__WEBPACK_IMPORTED_MODULE_4__["default"], {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 66,
+      columnNumber: 11
+    }
+  }))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    style: {
+      paddingTop: "22px",
+      paddingBottom: "52px",
+      textAlign: "center"
+    },
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 70,
+      columnNumber: 7
+    }
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("i", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 77,
+      columnNumber: 9
+    }
+  }, "(Replace below input with your email provider form)"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("br", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 78,
+      columnNumber: 9
+    }
+  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("br", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 79,
+      columnNumber: 9
+    }
+  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("input", {
+    style: {
+      outline: "1px solid black",
+      fontSize: "20px",
+      placeholder: "Your email",
+      padding: "6px",
+      marginRight: "12px"
+    },
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 80,
+      columnNumber: 9
+    }
+  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
+    className: "btn",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 90,
+      columnNumber: 9
+    }
+  }, "Subscribe now!"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("br", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 92,
+      columnNumber: 9
+    }
+  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("br", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 93,
+      columnNumber: 9
+    }
+  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(gatsby__WEBPACK_IMPORTED_MODULE_2__["Link"], {
+    to: "/archive",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 95,
+      columnNumber: 9
+    }
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
+    className: "button",
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 96,
+      columnNumber: 11
+    }
+  }, "See past issues"))));
+};
+IndexPageTemplate.propTypes = {
+  title: prop_types__WEBPACK_IMPORTED_MODULE_1___default.a.string,
+  subheading: prop_types__WEBPACK_IMPORTED_MODULE_1___default.a.string,
+  paragraph: prop_types__WEBPACK_IMPORTED_MODULE_1___default.a.string,
+  multipleItems: prop_types__WEBPACK_IMPORTED_MODULE_1___default.a.object
+};
+
+var IndexPage = function IndexPage(_ref2) {
+  var data = _ref2.data;
+  var frontmatter = data.markdownRemark.frontmatter;
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_Layout__WEBPACK_IMPORTED_MODULE_3__["default"], {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 114,
+      columnNumber: 5
+    }
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(IndexPageTemplate, {
+    title: frontmatter.title,
+    subheading: frontmatter.subheading,
+    paragraph: frontmatter.paragraph,
+    multipleItems: frontmatter.multipleItems,
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 115,
+      columnNumber: 7
+    }
+  }));
+};
+
+IndexPage.propTypes = {
+  data: prop_types__WEBPACK_IMPORTED_MODULE_1___default.a.shape({
+    markdownRemark: prop_types__WEBPACK_IMPORTED_MODULE_1___default.a.shape({
+      frontmatter: prop_types__WEBPACK_IMPORTED_MODULE_1___default.a.object
+    })
+  })
+};
+var _default = IndexPage;
+/* harmony default export */ __webpack_exports__["default"] = (_default);
+var pageQuery = "3265282182";
+/*
+
+image {
+          childImageSharp {
+            fluid(maxWidth: 2048, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+
+*/
+
+/*
+
+
+        mainpitch {
+          title
+          description
+        }
+
+
+*/
+
+;
+
+(function () {
+  var reactHotLoader = typeof reactHotLoaderGlobal !== 'undefined' ? reactHotLoaderGlobal.default : undefined;
+
+  if (!reactHotLoader) {
+    return;
+  }
+
+  reactHotLoader.register(IndexPageTemplate, "IndexPageTemplate", "C:\\Users\\GuilleHM\\Desktop\\newsletter-Gatsby-and-NetlifyCMS\\src\\templates\\index-page.js");
+  reactHotLoader.register(IndexPage, "IndexPage", "C:\\Users\\GuilleHM\\Desktop\\newsletter-Gatsby-and-NetlifyCMS\\src\\templates\\index-page.js");
+  reactHotLoader.register(pageQuery, "pageQuery", "C:\\Users\\GuilleHM\\Desktop\\newsletter-Gatsby-and-NetlifyCMS\\src\\templates\\index-page.js");
+  reactHotLoader.register(_default, "default", "C:\\Users\\GuilleHM\\Desktop\\newsletter-Gatsby-and-NetlifyCMS\\src\\templates\\index-page.js");
+})();
+
+;
+
+(function () {
+  var leaveModule = typeof reactHotLoaderGlobal !== 'undefined' ? reactHotLoaderGlobal.leaveModule : undefined;
+  leaveModule && leaveModule(module);
+})();
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./../../node_modules/webpack/buildin/harmony-module.js */ "./node_modules/webpack/buildin/harmony-module.js")(module)))
+
+/***/ }),
+
+/***/ "?3613":
+false,
+
+/***/ "?5e33":
+false
+
+})
+//# sourceMappingURL=cms.69f6b3e7fbd42fc67c54.hot-update.js.map
